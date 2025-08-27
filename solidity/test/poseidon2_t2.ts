192e9488370088ed55bb12e578d7b5e16804b2d7
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2025-08-27 13:32:40 +0200

    .

--- solidity/test/poseidon2_t2.ts
+++ solidity/test/poseidon2_t2.ts
@@ -13,9 +13,9 @@ describe("Poseidon2", async function () {
     const poseidon2 = await viem.deployContract("Poseidon2", undefined, {libraries: {
         "Poseidon2T2": poseidon2_lib.address, "Poseidon2T2opt": poseidon2_lib_opt.address
       }});
-    expect(await poseidon2.read.compress([[BigInt(0),BigInt(1)]])).to.be.equal(BigInt(0x1d01e56f49579cec72319e145f06f6177f6c5253206e78c2689781452a31878b));
+    expect(await poseidon2.read.compress([[BigInt(0),BigInt(1)]])).to.be.equal(BigInt("0x1d01e56f49579cec72319e145f06f6177f6c5253206e78c2689781452a31878b"));
 
-    expect(await poseidon2.read.compress_opt([[BigInt(0),BigInt(1)]])).to.be.equal(BigInt(0x1d01e56f49579cec72319e145f06f6177f6c5253206e78c2689781452a31878b));
+    expect(await poseidon2.read.compress_opt([[BigInt(0),BigInt(1)]])).to.be.equal(BigInt("0x1d01e56f49579cec72319e145f06f6177f6c5253206e78c2689781452a31878b"));
   });
 
 });
