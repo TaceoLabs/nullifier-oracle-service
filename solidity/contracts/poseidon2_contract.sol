20c426c46514510f7866598258641e14c043e593
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2025-08-27 14:20:20 +0200

    gas estimate

--- solidity/contracts/poseidon2_contract.sol
+++ solidity/contracts/poseidon2_contract.sol
@@ -6,13 +6,13 @@ import {Poseidon2T2} from "./poseidon2_t2.sol";
 import {Poseidon2T2opt} from "./poseidon2_t2_opt.sol";
 
 contract Poseidon2 {
-    function compress(uint256[2] memory inputs) public pure returns (uint256) {
-        return Poseidon2T2.compress(inputs);
+    uint256 public result;
+
+    function compress(uint256[2] memory inputs) public {
+        result = Poseidon2T2.compress(inputs);
     }
 
-    function compress_opt(
-        uint256[2] memory inputs
-    ) public pure returns (uint256) {
-        return Poseidon2T2opt.compress(inputs);
+    function compress_opt(uint256[2] memory inputs) public {
+        result = Poseidon2T2opt.compress(inputs);
     }
 }
--- solidity/test/poseidon2_t2.ts
+++ solidity/test/poseidon2_t2.ts
@@ -4,6 +4,7 @@ import { network } from "hardhat";
 
 describe("Poseidon2", async function () {
   const { viem } = await network.connect();
+  const publicClient = await viem.getPublicClient();
 
   it("Poseidon2 t2 compression kat", async function () {
   const poseidon2_lib = await viem.deployContract("contracts/poseidon2_t2.sol:Poseidon2T2");
@@ -16,12 +17,19 @@ describe("Poseidon2", async function () {
       }});
 
   console.log("Running Poseidon2 t2 compression kat...");
-  expect(await poseidon2.read.compress([[BigInt(0),BigInt(1)]])).to.be.equal(BigInt("0x1d01e56f49579cec72319e145f06f6177f6c5253206e78c2689781452a31878b"));
+  const hash1 = await poseidon2.write.compress([[BigInt(0),BigInt(1)]]);
+  const stats1 = await publicClient.waitForTransactionReceipt({ hash: hash1 });
+  const res1 = await poseidon2.read.result();
+  expect(res1).to.be.equal(BigInt("0x1d01e56f49579cec72319e145f06f6177f6c5253206e78c2689781452a31878b"));
   console.log("...done");
-
+  console.log("Gas used:", stats1.gasUsed);
 
   console.log("Running Poseidon2 t2 opt compression kat...");
-  expect(await poseidon2.read.compress_opt([[BigInt(0),BigInt(1)]])).to.be.equal(BigInt("0x1d01e56f49579cec72319e145f06f6177f6c5253206e78c2689781452a31878b"));
+  const hash2 = await poseidon2.write.compress_opt([[BigInt(0),BigInt(1)]]);
+  const stats2 = await publicClient.waitForTransactionReceipt({ hash: hash2 });
+  const res2 = await poseidon2.read.result();
+  expect(res2).to.be.equal(BigInt("0x1d01e56f49579cec72319e145f06f6177f6c5253206e78c2689781452a31878b"));
   console.log("...done");
+  console.log("Gas used:", stats2.gasUsed);
   });
 });
