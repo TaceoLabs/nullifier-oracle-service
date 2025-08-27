d032ef87e857f26757bff304db5d3885ef30d3c4
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2025-08-27 16:34:57 +0200

    feat: Replace the poseidon2 implementation with the faster one from the co-snark repo

--- poseidon2/src/params.rs
+++ poseidon2/src/params.rs
@@ -1,90 +1,49 @@
+pub mod bn254_t2;
+pub mod bn254_t3;
+pub mod bn254_t4;
+
 use ark_ff::PrimeField;
 
-use crate::utils;
-
-pub mod bn254;
-
+/// A parameter set containing the parameters for the Poseidon2 permutation.
 #[derive(Clone, Debug)]
-pub struct Poseidon2Params<F: PrimeField> {
-    pub(crate) t: usize, // statesize
-    pub(crate) d: usize, // sbox degree
-    pub(crate) rounds_f_beginning: usize,
-    pub(crate) rounds_p: usize,
-    #[allow(dead_code)]
-    pub(crate) rounds_f_end: usize,
-    pub(crate) rounds: usize,
-    pub(crate) mat_internal_diag_m_1: Vec<F>,
-    pub(crate) _mat_internal: Vec<Vec<F>>,
-    pub(crate) round_constants: Vec<Vec<F>>,
+pub struct Poseidon2Params<F: PrimeField, const T: usize, const D: u64> {
+    /// The amount of external rounds at the beginning of the permutation.
+    pub rounds_f_beginning: usize,
+    /// The amount of external rounds at the end of the permutation.
+    pub rounds_f_end: usize,
+    /// The amount of internal rounds.
+    pub rounds_p: usize,
+    /// The diagonal of t x t matrix of the internal permutation. Each element is taken minus 1 for more efficient implementations.
+    pub mat_internal_diag_m_1: &'static [F; T],
+    /// The round constants of the external rounds.
+    pub round_constants_external: &'static Vec<[F; T]>,
+    /// The round constants of the internal rounds.
+    pub round_constants_internal: &'static Vec<F>,
 }
 
-impl<F: PrimeField> Poseidon2Params<F> {
-    pub const INIT_SHAKE: &'static str = "Poseidon2";
-
-    pub fn new(
-        t: usize,
-        d: usize,
+impl<F: PrimeField, const T: usize, const D: u64> Poseidon2Params<F, T, D> {
+    pub(crate) fn new(
         rounds_f: usize,
         rounds_p: usize,
-        mat_internal_diag_m_1: &[F],
-        mat_internal: &[Vec<F>],
-        round_constants: &[Vec<F>],
+        mat_internal_diag_m_1: &'static [F; T],
+        round_constants_external: &'static Vec<[F; T]>,
+        round_constants_internal: &'static Vec<F>,
     ) -> Self {
-        assert!(d == 3 || d == 5 || d == 7 || d == 11);
+        assert!(T == 2 || T == 3 || ((T <= 24) && (T % 4 == 0)));
+        assert!(D % 2 == 1);
         assert_eq!(rounds_f % 2, 0);
-        let r = rounds_f / 2;
-        let rounds = rounds_f + rounds_p;
+        assert_eq!(round_constants_external.len(), rounds_f);
+        assert_eq!(round_constants_internal.len(), rounds_p);
+        let rounds_f_beginning = rounds_f / 2;
+        let rounds_f_end = rounds_f / 2;
 
-        Poseidon2Params {
-            t,
-            d,
-            rounds_f_beginning: r,
+        Self {
+            rounds_f_beginning,
+            rounds_f_end,
             rounds_p,
-            rounds_f_end: r,
-            rounds,
-            mat_internal_diag_m_1: mat_internal_diag_m_1.to_owned(),
-            _mat_internal: mat_internal.to_owned(),
-            round_constants: round_constants.to_owned(),
+            mat_internal_diag_m_1,
+            round_constants_external,
+            round_constants_internal,
         }
-    }
-
-    // Unused
-    pub fn equivalent_round_constants(
-        round_constants: &[Vec<F>],
-        mat_internal: &[Vec<F>],
-        rounds_f_beginning: usize,
-        rounds_p: usize,
-    ) -> Vec<Vec<F>> {
-        let mut opt = vec![Vec::new(); rounds_p + 1];
-        let mat_internal_inv = utils::mat_inverse(mat_internal);
-
-        let p_end = rounds_f_beginning + rounds_p - 1;
-        let mut tmp = round_constants[p_end].clone();
-        for i in (0..rounds_p - 1).rev() {
-            let inv_cip = Self::mat_vec_mul(&mat_internal_inv, &tmp);
-            opt[i + 1] = vec![inv_cip[0]];
-            tmp = round_constants[rounds_f_beginning + i].clone();
-            for i in 1..inv_cip.len() {
-                tmp[i].add_assign(&inv_cip[i]);
-            }
-        }
-        opt[0] = tmp;
-        opt[rounds_p] = vec![F::zero(); opt[0].len()]; // opt[0].len() = t
-
-        opt
-    }
-
-    pub fn mat_vec_mul(mat: &[Vec<F>], input: &[F]) -> Vec<F> {
-        let t = mat.len();
-        debug_assert!(t == input.len());
-        let mut out = vec![F::zero(); t];
-        for row in 0..t {
-            for (col, inp) in input.iter().enumerate() {
-                let mut tmp = mat[row][col];
-                tmp.mul_assign(inp);
-                out[row].add_assign(&tmp);
-            }
-        }
-        out
     }
 }
