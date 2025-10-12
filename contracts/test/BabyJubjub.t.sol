c47629bfed5347a0d3b1d6c37d32bee855ca52f4
Andrew Burger <andrewburger1991@gmail.com>
2025-10-12 17:44:03 +0200

    fixing test but checking for correct addition and failing

--- contracts/test/BabyJubjub.t.sol
+++ contracts/test/BabyJubjub.t.sol
@@ -10,6 +10,11 @@ contract BabyJubjubTest is Test {
     uint256 constant GEN_X = 5299619240641551281634865583518297030282874472190772894086521144482721001553;
     uint256 constant GEN_Y = 16950150798460657717958625567821834550301663161624707787222815936182638968203;
 
+    // Known values from circomlib/reference implementations
+    // 2G (generator doubled) - from circomlib test vectors
+    uint256 constant TWO_G_X = 17324563846726889236817837922625232543153115346355010501047597319863650987830;
+    uint256 constant TWO_G_Y = 20022170825455209233733649024450576091402881793145646502279487074566492066831;
+
     function setUp() public {
         babyJubjub = new BabyJubjub();
     }
@@ -31,20 +36,22 @@ contract BabyJubjubTest is Test {
     function testAddGeneratorToItself() public {
         (uint256 x, uint256 y) = babyJubjub.add(GEN_X, GEN_Y, GEN_X, GEN_Y);
         assertTrue(babyJubjub.isOnCurve(x, y));
+        assertEq(x, TWO_G_X);
+        assertEq(y, TWO_G_Y);
     }
 
     function testThreeTimes() public {
         // 2*Generator by adding generator to itself
-        (uint256 2Gx, uint256 2Gy) = babyJubjub.add(GEN_X, GEN_Y, GEN_X, GEN_Y);
-        assertTrue(babyJubjub.isOnCurve(2Gx, 2Gy));
+        (uint256 twoGx, uint256 twoGy) = babyJubjub.add(GEN_X, GEN_Y, GEN_X, GEN_Y);
+        assertTrue(babyJubjub.isOnCurve(twoGx, twoGy));
 
         // Add generator + 2*generator to get 3*generator
-        (uint256 3Gx, uint256 3Gy) = babyJubjub.add(GEN_X, GEN_Y, 2Gx, 2Gy);
+        (uint256 threeGx, uint256 threeGy) = babyJubjub.add(GEN_X, GEN_Y, twoGx, twoGy);
 
-        assertTrue(babyJubjub.isOnCurve(3Gx, 3Gy));
+        assertTrue(babyJubjub.isOnCurve(threeGx, threeGy));
 
         // Result should be different from both G and 2G
-        assertTrue(3Gx != GEN_X || 3Gy != GEN_Y);
-        assertTrue(3Gx != 2Gx || 3Gy != 2Gy);
+        assertTrue(threeGx != GEN_X || threeGy != GEN_Y);
+        assertTrue(threeGx != twoGx || threeGy != twoGy);
     }
 }
