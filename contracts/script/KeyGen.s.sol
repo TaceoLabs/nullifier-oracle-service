4c0aa562b9df8130051886324f93e2b7f4f0578c
Andrew Burger <andrewburger1991@gmail.com>
2025-10-12 17:32:34 +0200

    making KeyGen compile and adding BabyJubJub for addition

--- contracts/script/KeyGen.s.sol
+++ contracts/script/KeyGen.s.sol
@@ -17,12 +17,13 @@ contract KeyGenScript is Script {
         participants[0] = address(0x14dC79964da2C08b23698B3D3cc7Ca32193d9955);
         participants[1] = address(0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f);
         participants[2] = address(0xa0Ee7A142d267C1f36714E4a8F75612F20a79720);
+        address verifier = address(0x999);
 
 
         string memory filePath = "script/script-data/pubkey-list.hex";
         bytes memory peerKeys = vm.parseBytes(vm.readFile(filePath));
 
-        gen = new KeyGen(participants, 1, peerKeys);
+        gen = new KeyGen(verifier, participants, 1, peerKeys);
 
         vm.stopBroadcast();
         console.log("Contract deployed at:", address(gen));
