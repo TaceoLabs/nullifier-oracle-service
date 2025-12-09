3b1ba39cecd634e53a45fd032e1666be32edc3fa
0xThemis <118167989+0xThemis@users.noreply.github.com>
2025-12-09 15:48:04 +0100

    feat: contract now allows reshare

--- contracts/src/Types.sol
+++ contracts/src/Types.sol
@@ -4,14 +4,27 @@ pragma solidity ^0.8.20;
 /// @title Types Library
 /// @notice Defines common structs, enums, and constants for the project
 library Types {
+    // The roles of the nodes during key-gen. NOT_READY is the default value
+    enum KeyGenRole {
+        NOT_READY,
+        PRODUCER,
+        CONSUMER
+    }
+
     struct OprfPeer {
         bool isParticipant;
         uint256 partyId;
     }
 
+    struct RegisteredOprfPublicKey {
+        BabyJubJubElement key;
+        uint128 epoch;
+    }
+
     struct Round1Contribution {
+        // the commitment to the secret
         BabyJubJubElement commShare;
-        // Hash of the polynomial created by participant
+        // hash of the polynomial created by participant
         uint256 commCoeffs;
         // ephemeral public key for this round
         BabyJubJubElement ephPubKey;
@@ -30,9 +43,14 @@ library Types {
     }
 
     struct OprfKeyGenState {
+        mapping(address => KeyGenRole) nodeRoles;
+        uint256[] lagrangeCoeffs;
         Round1Contribution[] round1;
         SecretGenCiphertext[][] round2;
+        BabyJubJubElement[] shareCommitments;
         BabyJubJubElement keyAggregate;
+        uint128 numProducers;
+        uint128 generatedEpoch;
         bool[] round2Done;
         bool[] round3Done;
         bool round2EventEmitted;
@@ -57,7 +75,10 @@ library Types {
     event SecretGenRound1(uint160 indexed oprfKeyId, uint256 threshold);
     event SecretGenRound2(uint160 indexed oprfKeyId);
     event SecretGenRound3(uint160 indexed oprfKeyId);
-    event SecretGenFinalize(uint160 indexed oprfKeyId);
+    event SecretGenFinalize(uint160 indexed oprfKeyId, uint128 indexed epoch);
+    // events for reshare
+    event ReshareRound1(uint160 indexed oprfKeyId, uint256 threshold);
+    event ReshareRound3(uint160 indexed oprfKeyId, uint256[] lagrange);
     // event to delete created key
     event KeyDeletion(uint160 indexed oprfKeyId);
     // admin events
